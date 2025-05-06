655dd7672bbd9ebf3c280e88afb3cb9b599ea1db
Juan Sebastian Lopez Guzman <juanse150906@gmail.com>
2025-05-05 23:40:18 -0500

    [JuanSebastianLopez15] se agrego la clase Sprite, GraphicContainer, SpriteContainer y Soldado. Ademas se completo todo de las clases (Atributos y metodos). Por ultimo, se agrego la documentacion JavaDoc a las clases mencionadas

---
+++ src/autonoma/pulgaslocas/elements/Sprite.java
@@ -0,0 +1,89 @@
+package autonoma.pulgaslocas.elements;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Clase base para elementos gráficos. Maneja posición, tamaño y color.
+ * @author Juan Sebastian Lopez Guzman
+ * @version 1.0
+ * @since 2025-05-05
+ */
+public abstract class Sprite {
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected Color color;
+
+    /**
+     * Crea un Sprite con posición, tamaño y color
+     */
+    public Sprite(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    
+    /**
+     * Dibuja el sprite
+     */
+    public abstract void draw(Graphics g);
+    
+    /**
+     * Detecta colisión con otro sprite
+     * @return true si hay colisión
+     */
+    public boolean hit(Sprite other)
+    {
+        if(x < other.getX()+other.getWidth() &&
+           x+width > other.getX() &&
+           y < other.getY()+other.getHeight() &&
+           y+height > other.getY())
+            return true;
+        
+        return false;
+    }
+    // Getters y setters 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color=color;
+    }
+}
